0f8b5fa6193807a8cf2a3432853fd08249b06eb7
Christian <59421913+Cikmo@users.noreply.github.com>
2022-10-27 04:00:42 +0200

    clearn up main init

--- pnwapi/__init__.py
+++ pnwapi/__init__.py
@@ -1,18 +1,19 @@
-import importlib.metadata as _importlib_metadata
+import importlib.metadata as __importlib_metadata  # nopep8
+__version__ = __importlib_metadata.version(__package__ or __name__)  # nopep8
+del __importlib_metadata  # nopep8
 
-from . import pnwapi
-from . import objects
+from . import pnwapi as __pnwapi
+from . import objects as __objects
 
-__version__ = _importlib_metadata.version(__package__ or __name__)
+__all__ = ("init", "nations", "alliances", "close_connections")
 
+init = __pnwapi.Pnwapi.init
 
-init = pnwapi.Pnwapi.init
-
-nations = pnwapi.Interface(objects.Nation)
-alliances = pnwapi.Interface(objects.Alliance)
+nations = __pnwapi.Interface(__objects.Nation)
+alliances = __pnwapi.Interface(__objects.Alliance)
 
 
 async def close_connections():
     """Cleanly close the aiohttp session and the database connections. This should be called when the program is exiting."""
-    await pnwapi.Pnwapi._api.aiohttp_session.close()
-    await pnwapi.Tortoise.close_connections()
+    await __pnwapi.Pnwapi._api.aiohttp_session.close()
+    await __pnwapi.Tortoise.close_connections()
